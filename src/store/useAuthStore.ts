4126c33da3d322d22e9b61c0f8f38797bf136016
Abdug'affor Abdurahimov <abdugaffor.abdurakhimov@gmail.com>
2023-08-09 16:01:55 +0500

    WIP

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -1,16 +1,9 @@
 import { create } from "zustand";
 
-const testUser = {
-  email: "qwer",
-  key: "qwer",
-  name: "qwer",
-  secret: "qwer",
-  id: 0,
-};
 export const useAuthStore = create<{
   user: TUser | null;
   setUser: (user: TUser | null) => void;
 }>((set) => ({
-  user: testUser,
+  user: null,
   setUser: (user: TUser | null) => set({ user }),
 }));
